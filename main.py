79cee5076d2db2803231566f27f8b55afabaf1c7
Gabrielle Lepensky <129222364+glepensky@users.noreply.github.com>
2024-11-13 19:42:28 +0000

    practiced scripting

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import database
-
 # Create the database if it doesn't exist
 database.create_database()
-
 database.create_user()
-
 database.print_mydatabase()
---
+++ terminal_scripts.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a shell script!
+# You can write Terminal commands in here and they will 
+# all execute when you 'run' this script.
+echo Start shell script
+ls
+pwd
+echo End shell script
